aca2546374289bbb6ab7e02671d5d9f8832d1b97
nadezhda-vassova <nadezhda-vassova@users.noreply.github.com>
2014-10-19 12:36:20 +0300

    Movement of the monster - please check it and give me a feedback.

--- OOP-TeamWork/Structure/Monster.cs
+++ OOP-TeamWork/Structure/Monster.cs
@@ -1,10 +1,94 @@
-﻿namespace OOP_TeamWork
+﻿using System;
+
+namespace OOP_TeamWork
 {
     public abstract class Monster : Unit
     {
         public Monster(int x, int y, int width, int height, int health, int atack, int defense)
             : base(x, y, width, height, health, atack, defense)
         {
+        }
+
+
+        Random random = new Random();
+        int direction = random.Next(1, 5); // creates a number between 1 and 4
+        bool hasConflict = false;
+        bool hasWall = false;
+
+        private void MonsterRandomMovement(int direction)
+        {
+            while (true)
+                if (hasConflict = false)
+                {
+                    if (direction == 1)
+                    {
+                        this.MoveMonsterRight();
+                        if (hasWall == true)
+                        {
+                            hasConflict = true;
+                            Random random = new Random();
+                            direction = random.Next(1, 5); // creates a number between 1 and 4
+                            hasConflict = false;
+                        }
+                    }
+
+                    else if (direction == 2)
+                    {
+                        this.MoveMonsterDown();
+                        if (hasWall == true)
+                        {
+                            hasConflict = true;
+                            Random random = new Random();
+                            direction = random.Next(1, 5); // creates a number between 1 and 4
+                            hasConflict = false;
+                        }
+                    }
+
+                    else if (direction == 3)
+                    {
+                        this.MoveMonsterLeft();
+                        if (hasWall == true)
+                        {
+                            hasConflict = true;
+                            Random random = new Random();
+                            direction = random.Next(1, 5); // creates a number between 1 and 4
+                            hasConflict = false;
+                        }
+                    }
+
+                    else  if (direction == 4)
+                    {
+                        this.MoveMonsterUp();
+                        if (hasWall == true)
+                        {
+                            hasConflict = true;
+                            Random random = new Random();
+                            direction = random.Next(1, 5); // creates a number between 1 and 4
+                            hasConflict = false;
+                        }
+                    }
+                }
+        }
+
+        private Unit monster;
+        private void MoveMonsterRight()
+        {
+            this.monster.Move(1, 0); ;
+        }
+
+        private void MoveMonsterDown()
+        {
+            this.monster.Move(0, 1);
+        }
+
+        private void MoveMonsterUp()
+        {
+            this.monster.Move(0, -1);
+        }
+
+        private void MoveMonsterLeft()
+        {
+            this.monster.Move(-1, 0);
         }
     }
 }
